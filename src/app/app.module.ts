06829e94f8bad1b8baf314ca5722d904709012d0
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2021-09-13 21:06:37 -0300

    refactor files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PracticeMetricsComponent } from './comment-metrics/comment-metrics.component';
+import { PracticeMetricsComponent } from './metrics/comment-metrics/comment-metrics.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import { ChartsModule } from 'ng2-charts';
 import { DatePipe } from '@angular/common';
-import { VocabularyMetricsComponent } from './vocabulary-metrics/vocabulary-metrics.component';
+import { VocabularyMetricsComponent } from './metrics/vocabulary-metrics/vocabulary-metrics.component';
 
 @NgModule({
   declarations: [
